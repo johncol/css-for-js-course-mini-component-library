e4e83f48657fdb31c4288c9025c44a76570034ae
John <john.19col@gmail.com>
2023-12-27 21:40:48 -0800

    Add reset and global styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import { SampleButton } from "./components/SampleButton";
+import { ResetStyles } from "./global/ResetStyles";
+import { GlobalStyles } from "./global/GlobalStyles";
 
 const Home: React.FC = () => {
   return (
     <main>
       <h1>Hello</h1>
+
       <SampleButton>Boring</SampleButton>
       <SampleButton variant="info">Info</SampleButton>
       <SampleButton variant="danger">Danger</SampleButton>
+
+      <ResetStyles />
+      <GlobalStyles />
     </main>
   );
 };
